edc91e44fdb3829ec3873e4b6fc2e668cc55255d
Abishek Baiju <abhishekbaiju@protonmail.com>
2024-06-06 23:04:13 +0530

    Google OAuth authentication

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useGoogleLogin } from "@react-oauth/google";
+import { AuthContext } from "@/context/AuthContext";
+import RedirectToEmails from "./RedirectToEmails";
 
 export default function Login() {
+  const { token, setAccessTokenHandler } = useContext(AuthContext);
   const login = useGoogleLogin({
     scope: "https://www.googleapis.com/auth/gmail.readonly",
-    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onSuccess: (tokenResponse) => {
+      const access_token = tokenResponse.access_token;
+      setAccessTokenHandler(access_token);
+    },
   });
+
+  if (token !== "") {
+    return <RedirectToEmails />;
+  }
   return (
     <div className="flex flex-col items-center justify-around h-screen">
       <div className="">
@@ -18,10 +28,9 @@ export default function Login() {
         </button>
       </div>
       <div className="flex flex-col items-center gap-3">
-        {/* <label htmlFor="openai-key">Enter OpenAI API key</label> */}
         <input
           id="openai-api-key"
-          className="border border-blue-900 p-4 rounded-md transition focus:box-shadow-2xl"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 transition focus:box-shadow-2xl"
           type="text"
           name="openaiKey"
           placeholder="Enter OpenAI API key"
